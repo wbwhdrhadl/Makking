7b99fb71bdbeae8449ff2e63016db2069b771eb5
kimnayoung <kny200208@naver.com>
2024-05-12 23:51:19 +0900

    Fix:홈화면 로고

--- makking_app/lib/screens/home_screen.dart
+++ makking_app/lib/screens/home_screen.dart
@@ -5,64 +5,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 205, 254, 254),
+      backgroundColor: Color.fromARGB(255, 254, 255, 209),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/icons/makking_tv_broadcast_icon.png', // 새로운 아이콘 경로
+              '../assets/logo.png', // 새로운 아이콘 경로
               width: 200, // 아이콘 너비
               height: 200, // 아이콘 높이
             ),
             SizedBox(height: 20),
-            ElevatedButton(
+            TextButton(
               onPressed: () {
-                // 네이버 로그인 로직 추가
+                // 카카오 로그인 로직 추가 (나중에 구현)
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF1EC800), // 초록색 버튼
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                textStyle: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero, // 네모난 모양
-                ),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero, // 패딩 제거
+                minimumSize: Size(150, 50), // 최소 사이즈 설정
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(width: 10),
-                  Text('네이버로 계속하기'),
-                ],
+              child: Image.asset(
+                'assets/kakao_login_button.png', // 이미지 파일 경로
+                fit: BoxFit.cover, // 이미지를 버튼 크기에 맞게 조정
               ),
             ),
             SizedBox(height: 10),
-            ElevatedButton(
+            TextButton(
               onPressed: () {
-                // 카카오톡 로그인 로직 추가
+                // 네이버 로그인 로직 추가 (나중에 구현)
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFFFE812), // 노란색 버튼
-                foregroundColor: Colors.black,
-                padding: EdgeInsets.symmetric(horizontal: 17, vertical: 10),
-                textStyle: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero, // 네모난 모양
-                ),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero, // 패딩 제거
+                minimumSize: Size(150, 50), // 최소 사이즈 설정
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(width: 10),
-                  Text('카카오톡으로 계속하기'),
-                ],
+              child: Image.asset(
+                'assets/naver_login.png', // 이미지 파일 경로
+                fit: BoxFit.cover, // 이미지를 버튼 크기에 맞게 조정
               ),
             ),
             SizedBox(height: 20),
