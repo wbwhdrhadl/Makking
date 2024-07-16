0fe9d00f9e89606125453e1ee88d4e90a908f33a
김나영 <kny200208@naver.com>
2024-07-17 01:41:25 +0900

    Revert "Update server.js"
    This reverts commit 2248a5e95c5e2ba79fcba686450d3b0f218bb894.

--- node_backend/server.js
+++ node_backend/server.js
@@ -4,27 +4,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const socketIo = require('socket.io');
 const axios = require('axios');
-const http = require('http');
 require("dotenv").config();
 
 const app = express();
-const server = http.createServer(app);
+const server = require('http').createServer(app);
 const io = socketIo(server);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// MongoDB 연결 설정
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/makking';
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB 연결 성공"))
-.catch((err) => console.log("MongoDB 연결 오류:", err));
-
-// Socket.IO 이벤트 핸들러
 io.on('connection', (socket) => {
   console.log('A new client has connected!');
 
@@ -45,16 +34,13 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-// 라우터 설정
-const chatRouter = require("./routes/broaddata.js");
-const s3Router = require("./routes/s3.js");
-const userRouter = require("./routes/User.js");
-const kakaoUserRouter = require("./routes/kakaoUser.js");
-
-app.use("/", chatRouter);
-app.use("/", s3Router);
-app.use("/", kakaoUserRouter);
-app.use("/", userRouter);
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/makking';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5001;
-server.listen(PORT, "0.0.0.0", () => console.log(`서버가 포트 ${PORT}에서 시작되었습니다`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
