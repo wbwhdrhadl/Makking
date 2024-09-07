730bf10c9d50fe1b9caebab0df3b73867265bee0
LEE DA EUN <112735794+wbwhdrhadl@users.noreply.github.com>
2024-09-07 23:53:40 +0900

    test: google-text

--- makking_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ makking_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,18 +7,16 @@ import Foundation
 
 import file_selector_macos
 import flutter_webrtc
-import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import video_player_avfoundation
-import wakelock_plus
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
